8a30280bfa9dbcf7099a81de464ecccede85e9da
Reynell Quevedo <reynell4587@hotmail.com>
2023-10-13 23:28:43 -0500

    Exponentiation II solved

---
+++ Gold/Math/Modular Arithmetic/CSES_ExponentiationII.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define MINF INT_MIN
+#define ll long long
+#define PB push_back 
+#define PF push_front
+#define P_F pop_front
+#define P_B pop_back
+#define F front
+#define B back
+#define f first
+#define s second
+#define MP make_pair
+#define FOR(i, a, b) for(ll i = a; i < b; i++)
+#define FORI(i, a, b) for(ll i = a; i >= b; i--)
+
+using namespace std;
+const ll MOD = 1000000007;
+
+ll binpow(ll a, ll b, ll MOD) {
+    a %= MOD;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1) res = res * a % MOD;
+        a = a * a % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        int a, b, c; cin >> a >> b >> c;
+        cout << binpow(a, binpow(b, c, MOD - 1), MOD) << "\n";
+    }
+    
+
+    
+    return 0;
+}

60769e20d06ba42e0bab8d001f555124be79a218
Reynell Quevedo <reynell4587@hotmail.com>
2023-10-24 09:57:56 -0500

    CSES_SumOfDivisors solved

--- Gold/Math/Divisibility/CSES_SumOfDivisors.cpp
+++ Gold/Math/Divisibility/CSES_SumOfDivisors.cpp
@@ -3,49 +3,26 @@
 #define ll long long
 
 using namespace std;
-const int MAXN = 1e6 + 10;
-
-vector<int> primes(){
-    vector<int> primes;
-    vector<bool> prime(MAXN + 1);
-    for(ll i = 2; i <= MAXN ; ++i ){
-        if(!prime[i]){
-            primes.push_back(i);
-            for(ll j = i*i ; j<= MAXN ; j+=i){
-                prime[j] = true;
-            }
-        }
-    } 
-    return primes;
-}
-
-map<ll, int> primeDecomposition(ll num, vector<int>& p){
-    map<ll, int> mp;
-    for(int i = 0 ; i < p.size() && num > 1 ; ++i){
-        while(num % p[i] == 0){
-            mp[p[i]]++;
-            num/=p[i];
-        }
-    }
-    if(num != 1){
-        mp[num]++;
-    }
-    return mp;
-}
 const ll DIV = 500000004ll;
 const ll MOD = 1000000007ll;
 
 int main()
 {
     ll N; cin >> N;
-    ll res = 1ll;
+    ll res = 0ll;
     ll rest = 0;
-    for(int i = 1 ; i*i <= N ; ++i){
-        int k = N/i;
+    for(ll i = 1 ; i*i <= N ; ++i){
+        ll k = N/i;
+    
+        k = k%MOD;
         rest= (rest + i)%MOD;
-        res= (res + i*(k-1))%MOD ;
-        res = ( res + ((k*(k + 1))%MOD)*DIV ) %MOD;
+        res = (res + i*k)%MOD ;
+        res = (res - i*(i-1) + MOD)%MOD ;
+        res = (res + (((k*(k + 1))%MOD)*DIV)%MOD ) %MOD;
         res = (res - rest + MOD)%MOD;
+        
+        //rest = rest + i;
+        //res = res + ( i*k - i*(i-1) + (k*(k+1))/2 - rest); 
     } 
     cout << res << "\n";
     return 0;
